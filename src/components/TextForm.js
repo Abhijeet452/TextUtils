42ab2307d33074ec5ca28b701ebf1958440ce8d4
Abhijeet452 <iit2020214@iiita.ac.in>
2021-10-01 22:46:55 +0530

    Added Some more utility

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,33 +2,33 @@ import React, { useState } from 'react'
 export default function TextForm(props) {
     const [text, setText] = useState('');
     const handleUpClick = () => {
-        let newText = text.toUpperCase();
-        setText(newText);
         if (text.length === 0) {
             props.showAlert("Enter Some Text First", "danger");
         }
         else {
+            let newText = text.toUpperCase();
+            setText(newText);
             props.showAlert("Converted to Upper Case", "success");
         }
     }
     const handleLoClick = () => {
-        let newText = text.toLowerCase();
-        setText(newText);
         if (text.length === 0) {
             props.showAlert("Enter Some Text First", "danger");
         }
         else {
+            let newText = text.toLowerCase();
+            setText(newText);
             props.showAlert("Converted to Lower Case", "success");
         }
 
     }
     const handleOnClear = () => {
-        let newText = '';
-        setText(newText);
         if (text.length === 0) {
             props.showAlert("TextBox is Empty", "warning");
         }
         else {
+            let newText = '';
+            setText(newText);
             props.showAlert("Text Cleared", "success");
         }
     }
@@ -37,33 +37,23 @@ export default function TextForm(props) {
             props.showAlert("Nothing to copy", "danger");
         }
         else {
-            let newText = document.getElementById("myBox");
-            newText.select();
-            navigator.clipboard.writeText(newText.value);
+            navigator.clipboard.writeText(text);
             props.showAlert("Copied Text", "success");
         }
     }
     const handleOnExtraSpaces = () => {
-        let newText = text.split(/[ ]+/);
-        setText(newText.join(" "));
         if (text.length === 0) {
             props.showAlert("Enter Some Text First", "danger");
         }
         else {
+            let newText = text.split(/[ ]+/);
+            setText(newText.join(" "));
             props.showAlert("Removed Extra Spaces", "success");
         }
 
     }
-    function count_words(str1) {
-        // str1 = document.getElementById("InputText").value;
-        //exclude  start and end white-space
-        str1 = str1.replace(/(^\s*)|(\s*$)/gi, "");
-        //convert 2 or more spaces to 1  
-        str1 = str1.replace(/[ ]{2,}/gi, " ");
-        // exclude newline with a start spacing  
-        str1 = str1.replace(/\n /, "\n");
-        return str1.split(' ').length;
-    }
+    let wordCount=text.split(/\s+/).filter((element)=>{ return element.length!==0}).length;
+
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
@@ -81,24 +71,27 @@ export default function TextForm(props) {
         backgroundColor: props.mode === 'dark' ? 'rgb(255 255 255 / 15%)' : 'rgb(222 222 222)',
         color: props.mode === 'dark' ? 'White' : 'Black'
     };
+    const dis=()=>{
+        return text.length===0;
+    }
     return (
         <>
             <div className="container">
-                <h2>{props.heading}</h2>
+                <h2 className="mb-4">{props.heading}</h2>
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10" style={mystls}></textarea>
                 </div>
                 <div className="d-flex justify-content-center flex-column">
-                    <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
-                    <button className="btn btn-primary my-2 mx-2" onClick={handleLoClick}>Convert to Lower Case</button>
-                    <button className="btn btn-primary my-2 mx-2" onClick={handleOnClear}>Clear Text</button>
-                    <button className="btn btn-primary my-2 mx-2" onClick={handleOnCopy}>Copy Text</button>
-                    <button className="btn btn-primary my-2 mx-2" onClick={handleOnExtraSpaces}>Remove Extra Spaces</button>
+                    <button disabled={dis()} className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
+                    <button disabled={dis()} className="btn btn-primary my-2 mx-2" onClick={handleLoClick}>Convert to Lower Case</button>
+                    <button disabled={dis()} className="btn btn-primary my-2 mx-2" onClick={handleOnClear}>Clear Text</button>
+                    <button disabled={dis()} className="btn btn-primary my-2 mx-2" onClick={handleOnCopy}>Copy Text</button>
+                    <button disabled={dis()} className="btn btn-primary my-2 mx-2" onClick={handleOnExtraSpaces}>Remove Extra Spaces</button>
                 </div>
             </div>
             <div className="container my-3">
                 <h1>Text Summary</h1>
-                <h4>{text.length} characters {count_words(text)} words {senscount} sentences {text.length / 125} minutes read</h4>
+                <h4>{text.length} characters {wordCount} words {senscount} sentences {(text.split(' ').filter((element)=>{return element.length!==0}).length) / 125} minutes read</h4>
                 <h2>Preview</h2>
                 <h5>{text.length > 0 ? text : "Enter Some Text above to preview here :)"}</h5>
             </div>
